0684f3c072dbb70e9ede53983b407d8bca5bc4de
park <hanchon1@gmail.com>
2016-06-14 15:56:37 +0900

    createdraem.jsp ,DreamController 꿈 등록 부분 변경(꿈등록,보상등록 메소드 결합)

--- moreDream01/src/main/java/controller/DreamController.java
+++ moreDream01/src/main/java/controller/DreamController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -24,24 +25,26 @@ import dao.DreamService;
  * DreamController 주석 추가
  */
 public class DreamController extends MultiActionController{
-
+	
 	private DreamService dreamService;
 	private String path;
 
 	public void setPath(String path) {
 		this.path = path;
 	}
-
+	
 	public void setDreamService(DreamService dreamService) {
 		this.dreamService = dreamService;
 	}
-
-	//꿈 신청
-	public ModelAndView requestDream(HttpServletRequest request,HttpServletResponse response, HttpSession session, DreamVO pvo)
+	
+	//꿈 신청 
+	//160614(한천)
+	   public ModelAndView requestDream(HttpServletRequest request,HttpServletResponse response, HttpSession session,DreamVO pvo)
 			throws Exception{
 		System.out.println("requestDream GO!!");
-
+//		DreamVO pvo = new DreamVO(0, null, request.getParameter("category"), " ", request.getParameter("titleDream"), request.getParameter("detailDream"), "", Integer.parseInt(request.getParameter("targetFund")), request.getParameter("startDate"), request.getParameter("endDate"));
 		// 1. 업로드된 파일명을 받아온다.
+//		pvo.setMultipartFile(multipartFile);
 		MultipartFile mFile = pvo.getMultipartFile();
 		System.out.println("MultipartFile :: " + mFile);
 
@@ -67,85 +70,109 @@ public class DreamController extends MultiActionController{
 		}
 
 		System.out.println("꿈 신청 Go");
-
+		
 		MemberVO rmvo =   (MemberVO) session.getAttribute("mvo");
 		pvo.setMemberVO(rmvo);//세션 값 주입
-
+		
 		System.out.println("1"+pvo);
 		dreamService.requestDream(pvo);
 		System.out.println("2"+pvo);
-
-
-		return new ModelAndView("registerReward", "dreamId", pvo.getDreamId());
+		String[] rewardInfoArr = request.getParameterValues("rewardInfo");
+		String[] rewardGuideArr = request.getParameterValues("rewardGuide");
+		String[] stockArr = request.getParameterValues("stock");
+		
+		
+		
+		for(String rewardInfo: rewardInfoArr)
+			System.out.println("rewardInfo :: "+rewardInfo);
+		for(String rewardGuide: rewardGuideArr)
+			System.out.println("rewardGuide :: "+rewardGuide);
+		for(String stock: stockArr)
+			System.out.println("stock :: "+stock);
+		List<RewardVO> rewardList = new ArrayList<RewardVO>();
+		
+		for(int i=0;i<rewardInfoArr.length;i++){
+			RewardVO rpvo = new RewardVO();
+			rpvo.setRewardId(0);
+			rpvo.setDreamVO(pvo);
+			rpvo.setRewardInfo(rewardInfoArr[i]);
+			rpvo.setRewardGuide(Integer.parseInt(rewardGuideArr[i]));
+			rpvo.setStock(Integer.parseInt(stockArr[i]));
+			rewardList.add(rpvo);
+		}
+		for(RewardVO rvo : rewardList){
+			dreamService.registerReward(rvo);
+		}
+		return new ModelAndView("redirect:/index.jsp");
 	}
-
-	//보상 등록
+	
+	/*//보상 등록
 	public ModelAndView registerReward(HttpServletRequest request,HttpServletResponse response, RewardVO pvo)
 			throws Exception{
 		System.out.println("registerReward GO!!");
 		System.out.println("RewardVO :: "+pvo);
-
+		
 		DreamVO dreamVO = new DreamVO();
-
-
+		
+		
 		dreamVO.setDreamId(Integer.parseInt(request.getParameter("dreamId")));
-
+		
 		pvo.setDreamVO(dreamVO);
-
+		
 		dreamService.registerReward(pvo);
-
+		
 		return new ModelAndView("index");
-	}
-
-
-
+	}*/
+	
+	
+	
 	//꿈 목록
 	public ModelAndView getAllListDream(HttpServletRequest request,HttpServletResponse response)
 			throws Exception{
-		String num = request.getParameter("filter");
-		if(num==null){
-			num = "1";
-		}
-		List<DreamVO> dreamList = dreamService.getListDream(num);
-		System.out.println(dreamList);
-		return new ModelAndView("./finddream", "dreamList", dreamList);
+				String num = request.getParameter("filter");
+				if(num==null){
+					num = "1";
+				}
+				List<DreamVO> dreamList = dreamService.getListDream(num);
+				System.out.println(dreamList);
+				return new ModelAndView("./finddream", "dreamList", dreamList);
 	}
-
+	
 	//관리자 꿈 목록 보기
 	public ModelAndView getAllListDreamForAdmin(HttpServletRequest request,HttpServletResponse response)
 			throws Exception{
-
-		List<DreamVO> adminDreamList = dreamService.getAllListDreamForAdmin();
-
-		System.out.println(adminDreamList);
-		return new ModelAndView("./admindreampage", "adminDreamList", adminDreamList);
+					
+			List<DreamVO> adminDreamList = dreamService.getAllListDreamForAdmin();
+			
+			System.out.println(adminDreamList);
+			return new ModelAndView("./admindreampage", "adminDreamList", adminDreamList);
 	}
-
+	
 	//카테고리 검색
 	public ModelAndView selectByCategory(HttpServletRequest request,HttpServletResponse response)
-			throws Exception{
+	throws Exception{
 		String category = request.getParameter("category");
 		System.out.println(category);
 		List<DreamVO> list = dreamService.selectByCategory(category);
 		System.out.println(list);
 		return new ModelAndView("./test/selectByCategory", "list", list);
 	}
-
+	
 	// Admin 꿈 승인하기
 	public ModelAndView confirmRequestDream(HttpServletRequest request,HttpServletResponse response)
 			throws Exception{
-		int dreamId = Integer.parseInt(request.getParameter("dreamId"));
-		String confirmRequestDream = request.getParameter("confirmRequestDream");
-		System.out.println("confirmRequestDream() :: "+dreamId + " "+confirmRequestDream);
-
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("dreamId", dreamId);
-		map.put("confirm", confirmRequestDream);
-
-		dreamService.confirmRequestDream(map);
-
-		return new ModelAndView("redirect:/dream.do?command=getAllListDreamForAdmin");
-	}
+				int dreamId = Integer.parseInt(request.getParameter("dreamId"));
+				String confirmRequestDream = request.getParameter("confirmRequestDream");
+				System.out.println("confirmRequestDream() :: "+dreamId + " "+confirmRequestDream);
+				
+				Map<String, Object> map = new HashMap<String, Object>();
+				map.put("dreamId", dreamId);
+				map.put("confirm", confirmRequestDream);
+				
+				dreamService.confirmRequestDream(map);
+				
+				return new ModelAndView("redirect:/dream.do?command=getAllListDreamForAdmin");
+		}
 	//추가 160614
 	// :: getDetailDreamByDreamId - 꿈 상세보기
 	public ModelAndView getDetailDreamByDreamId(HttpServletRequest request, HttpServletResponse response)
--- moreDream01/src/main/webapp/createdream.jsp
+++ moreDream01/src/main/webapp/createdream.jsp
@@ -25,7 +25,80 @@
 <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
 <script type="text/javascript">
 	var now = new Date();
-
+	function create(){
+		var rewardInfoArr = document.frm.rewardInfo;
+		if(document.frm.category.value==""){
+			alert('카테고리를 선택해주세요');
+			document.frm.category.focus();
+			return false;
+		}
+		else if(document.frm.titleDream.value==""){
+			alert('꿈 제목을 입력해주세요.');
+			document.frm.titleDream.focus();
+			return false;
+		}
+		else if(document.frm.detailDream.value==""){
+			alert('꿈 내용을 입력해주세요.');
+			document.frm.detailDream.focus();
+			return false;
+		}
+		else if(document.frm.multipartFile.value==""){
+			alert('파일을 반드시 등록해야합니다.');
+			document.frm.multipartFile.focus();
+			return false;
+		}
+		else if(document.frm.targetFund.value==""||document.frm.targetFund.value=="0"){
+			alert('목표금액을 설정해주세요 ');
+			document.frm.targetFund.focus();
+			return false;
+		}else if(document.frm.targetFund.value<100000){
+			alert('목표금액이 10만원 보다 작습니다. 10만원 이상의 목표금액을 입력하세요.');
+			document.frm.targetFund.focus();
+			return false;
+		}else if(document.frm.startDate.value==""){
+			alert('시작일자를 선택하세요.');
+			document.frm.startDate.focus();
+			return false;
+		}else if(document.frm.endDate.value==""){
+			alert('마감일자를 선택하세요.');
+			document.frm.endDate.focus();
+			return false;
+		}else{
+			if(rewardInfoArr.length==undefined){//보상내역이 한개일때
+				if(document.frm.rewardInfo.value==""){
+					alert('보상정보를 입력하세요.');
+					document.frm.rewardInfo.focus();
+					return false;
+				}else if(document.frm.rewardGuide.value==""){
+					alert('보상 기준액 입력하세요.');
+					document.frm.rewardGuide.focus();
+					return false;
+				}else if(document.frm.stock.value==""){
+					alert('보상 수량 입력하세요.');
+					document.frm.stock.focus();
+					return false;
+				}
+			}else if(rewardInfoArr.length>=2){
+				var rewardGuideArr = document.frm.rewardGuide;
+				var stockArr = document.frm.stock;
+				for(i=0;i<=rewardInfoArr.length;i++){
+					if(rewardInfoArr[i].value==""){
+						alert('보상정보를 입력하세요.');
+						rewardInfoArr[i].focus();
+						return false;
+					}else if(rewardGuideArr[i].value==""){
+						alert('보상 기준액 입력하세요.');
+						rewardGuideArr[i].focus();
+						return false;
+					}else if(stockArr[i].value==""){
+						alert('보상 수량 입력하세요.');
+						stockArr[i].focus();
+						return false;
+					}
+				}
+			}
+		}
+	}
 	$(function() {
 		$('#datePicker1').datepicker(
 				{
@@ -43,6 +116,12 @@
 							'8월', '9월', '10월', '11월', '12월' ],
 					minDate : now
 				});
+	});
+	$(document).ready(function(){
+		$('#plusBtn').click(function(){
+			$(this).before(
+					"<div class='col-md-4' style='margin-top:10px'><textarea class='form-control' rows='5' placeholder='보상 정보' name='rewardInfo'></textarea><br><input type='text' class='form-control' placeholder='금액 기준' name='rewardGuide'><br>	<input type='text' class='form-control' placeholder='재고' name='stock'></div>");
+		});
 	});
 </script>
 <!--[if lt IE 9]>
@@ -81,7 +160,7 @@
 		</div>
 	</section>
 	<!--/#page-breadcrumb-->
-	<form action="./dream.do" method="post" enctype="multipart/form-data">
+	<form name="frm" action="./dream.do" method="post" enctype="multipart/form-data" onsubmit="return create();">
 		<section id="shortcodes">
 			<div class="container">
 				<div id="feature-container">
@@ -93,7 +172,7 @@
 
 
 							카테고리 선택 <select class="form-control" name="category">
-								<option>======선택해주세요=======</option>
+								<option value="">======선택해주세요=======</option>
 								<option value="영화">영화</option>
 								<option value="출판">출판</option>
 								<option value="음악">음악</option>
@@ -104,7 +183,6 @@
 							<div align="center">
 								<h2 class="page-header" align="left">꿈 제목</h2>
 								<textarea class="form-control" rows="2" name="titleDream"></textarea>
-
 
 								<h2 class="page-header" align="left">꿈 내용</h2>
 								<textarea class="form-control" rows="5" name="detailDream"></textarea>
@@ -119,10 +197,27 @@
 							<div class="input-group">
 								<span class="input-group-addon">$</span> <input type="text"
 									class="form-control"
-									aria-label="Amount (to the nearest dollar)" name="targetFund">
+									aria-label="Amount (to the nearest dollar)" name="targetFund" placeholder="10만원 이상의 금액을 입력하세요.">
 								<span class="input-group-addon">원</span>
 							</div>
-
+							&nbsp;
+							<!-- 보상정보 추가 -->
+							<h2 class="page-header">보상 정보 등록</h2>
+							<div class="col-md-12">
+	
+								<div class="col-md-4" style='margin-top:10px'>
+									<textarea class="form-control" rows="5" placeholder="보상 정보" name="rewardInfo"></textarea><br>
+									<input type="text" class="form-control" placeholder="금액 기준" name="rewardGuide"><br>
+									<input type="text" class="form-control" placeholder="재고" name="stock">
+								</div>
+								
+								<button type="button" class="btn btn-default btn-lg" id="plusBtn">
+									<span class="glyphicon glyphicon-plus" aria-hidden="true" style="width:20px;height:20px"></span>
+								</button>
+								
+								&nbsp;
+							</div>
+							&nbsp;
 							<h2 class="page-header">꿈 기한</h2>
 							&nbsp;
 							<div class="col-md-12">
